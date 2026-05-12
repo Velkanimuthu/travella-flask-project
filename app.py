cadbb12fc7f51d3d3cbe2d402ea615905866c3d2
velkani <velmuthu528494@gmail.com>
2026-05-12 22:29:59 +0530

    Fix MYSQLPORT environment variable handling

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_db():
             user=os.getenv("MYSQLUSER"),
             password=os.getenv("MYSQLPASSWORD"),
             database=os.getenv("MYSQLDATABASE"),
-            port=os.getenv("MYSQLPORT")
+            port=int(os.getenv("MYSQLPORT", 3306))
         )
         return conn
     except Error as e:
